719a7f99ddadc788fdcad6d9d6d417bd83714b18
freedomcondor <zhuweixu_harry@126.com>
2017-11-22 10:49:08 +0100

    test

--- loopFunction/build/CMakeFiles/testLoopFunction.dir/DependInfo.cmake
+++ loopFunction/build/CMakeFiles/testLoopFunction.dir/DependInfo.cmake
@@ -4,13 +4,13 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/harry/Desktop/argosGoAround/loopFunction/testLoopFunction.cpp" "/Users/harry/Desktop/argosGoAround/loopFunction/build/CMakeFiles/testLoopFunction.dir/testLoopFunction.cpp.o"
+  "/home/harry/argosGoAround/loopFunction/testLoopFunction.cpp" "/home/harry/argosGoAround/loopFunction/build/CMakeFiles/testLoopFunction.dir/testLoopFunction.cpp.o"
   )
-set(CMAKE_CXX_COMPILER_ID "Clang")
+set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "/usr/local/include"
+  "/usr/include/lua5.2"
   )
 
 # Targets to which this target links.
